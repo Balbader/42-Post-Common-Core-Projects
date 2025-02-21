5c42c9f7c60be2de2c5e5329893c3286a9f2d74b
balbader <basilalbapro@gmail.com>
2025-02-21 13:57:45 +0100

    back to go

---
+++ go/to_do_list_app/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to the To-Do List App")
+	fmt.Println("Watch out for the evil boss!")
+	fmt.Println("What do you want to do next?")
+	fmt.Println("1. Add a new task")
+	fmt.Println("2. View all tasks")
+	fmt.Println("3. Mark a task as completed")
+	fmt.Println("4. Exit")
+}
